3817fd6da26c3dc5a82b1de93407b53c491a75d3
sub101 <subin10@nate.com>
2023-05-04 15:20:20 +0900

    도전과제

--- app/src/main/java/org/android/go/sopt/Main2Activity.kt
+++ app/src/main/java/org/android/go/sopt/Main2Activity.kt
@@ -9,7 +9,7 @@ import org.android.go.sopt.fragment.HomeFragment
 import org.android.go.sopt.fragment.MypageFragment
 import org.android.go.sopt.fragment.SearchFragment
 
-class Main2Activity: AppCompatActivity(){
+class Main2Activity : AppCompatActivity() {
     lateinit var binding: ActivityMain2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,41 +17,39 @@ class Main2Activity: AppCompatActivity(){
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val a = HomeFragment()
-
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
-        if(currentFragment == null){
+        if (currentFragment == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.fcv_main,HomeFragment()).commit()
+                .replace(R.id.fcv_main, HomeFragment()).commit()
         }
 
-        binding.bnvMain.setOnItemSelectedListener{ item ->
+        binding.bnvMain.setOnItemSelectedListener { item ->
 
-                when(item.itemId){
-                    R.id.menu_home -> {
-                        changeFragment(HomeFragment())
-                        return@setOnItemSelectedListener true
-                    }
-                    R.id.menu_search -> {
-                        changeFragment(SearchFragment())
-                        return@setOnItemSelectedListener true
-                    }
-                    R.id.menu_gallery -> {
-                        changeFragment(GalleryFragment())
-                        return@setOnItemSelectedListener true
-                    }
-                    else -> {
-                        changeFragment(MypageFragment())
-                        return@setOnItemSelectedListener true
-                    }
+            when (item.itemId) {
+                R.id.menu_home -> {
+                    changeFragment(HomeFragment())
+                    return@setOnItemSelectedListener true
                 }
+                R.id.menu_search -> {
+                    changeFragment(SearchFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.menu_gallery -> {
+                    changeFragment(GalleryFragment())
+                    return@setOnItemSelectedListener true
+                }
+                else -> {
+                    changeFragment(MypageFragment())
+                    return@setOnItemSelectedListener true
+                }
+            }
 
         }
     }
 
-    private fun changeFragment(fragment: Fragment){
+    private fun changeFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fcv_main,fragment)
+            .replace(R.id.fcv_main, fragment)
             .commit()
     }
 }
--- app/src/main/java/org/android/go/sopt/fragment/MypageFragment.kt
+++ app/src/main/java/org/android/go/sopt/fragment/MypageFragment.kt
@@ -1,11 +1,13 @@
 package org.android.go.sopt.fragment
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.android.go.sopt.MainActivity
 import org.android.go.sopt.databinding.FragmentMypageBinding
 
 class MypageFragment : Fragment() {
@@ -13,7 +15,6 @@ class MypageFragment : Fragment() {
     private val binding: FragmentMypageBinding
         get() = requireNotNull(_binding) { "binding is null" }
 
-    lateinit var alertDialog: AlertDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +26,8 @@ class MypageFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.btnMypageLogout.setOnClickListener{
+
+        binding.btnMypageLogout.setOnClickListener {
             showLogoutDialog()
         }
     }
@@ -34,11 +36,12 @@ class MypageFragment : Fragment() {
         val builder = AlertDialog.Builder(requireContext())
         builder.setTitle("Logout")
         builder.setMessage("로그아웃 하시겠습니까?")
-        builder.setPositiveButton("확인") { dialog, which ->
-            // ok button
+        builder.setPositiveButton("Yes") { dialog, which ->
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
         }
-        builder.setNegativeButton("취소") { dialog, which ->
-            // cancel button
+        builder.setNegativeButton("No") { dialog, which ->
+            dialog.dismiss()
         }
         val dialog = builder.create()
         dialog.show()
