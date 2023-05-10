4074a0a9d935a3af3f06cc15fe52539e7ea116c2
sub101 <subin10@nate.com>
2023-05-10 16:51:16 +0900

    4차세미나 실습

--- app/src/main/java/org/android/go/sopt/fragment/GalleryFragment.kt
+++ app/src/main/java/org/android/go/sopt/fragment/GalleryFragment.kt
@@ -5,22 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.android.go.sopt.GalleryAdapter
+import org.android.go.sopt.R
+import org.android.go.sopt.databinding.FragmentGalleryBinding
+import org.android.go.sopt.databinding.FragmentHomeBinding
 
 class GalleryFragment : Fragment() {
+
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding: FragmentGalleryBinding
+        get() = requireNotNull(_binding) { "binding is null" }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.pagerGallery.adapter = GalleryAdapter().apply {
+            setItemList(listOf(R.drawable.dog,R.drawable.dog,R.drawable.dog))
+        }
     }
 
     override fun onDestroyView() {
+        _binding = null
         super.onDestroyView()
     }
 }
