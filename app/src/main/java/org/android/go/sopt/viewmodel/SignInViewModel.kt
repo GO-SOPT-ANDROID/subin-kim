f92304e8f10c19930578c938b466c8c77d391bc7
sub101 <subin10@nate.com>
2023-05-20 13:33:24 +0900

    reset

---
+++ app/src/main/java/org/android/go/sopt/viewmodel/SignInViewModel.kt
@@ -0,0 +1,35 @@
+package org.android.go.sopt.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class SignInViewModel : ViewModel() {
+    val signInResult: MutableLiveData<ResponseSignInDto> = MutableLiveData()
+
+    fun signIn(id: String, password: String) {
+        signInService.signIn(
+            RequestSignInDto(
+                id,
+                password
+            )
+        ).enqueue(object : Callback<ResponseSignInDto> {
+            override fun onResponse(
+                call: Call<ResponseSignInDto>,
+                response: Response<ResponseSignInDto>,
+            ) {
+                if (response.isSuccessful) {
+                    signInResult.value = response.body()
+                } else {
+                    // TODO: 에러 처리
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseSignInDto>, t: Throwable) {
+                // TODO: 에러 처리
+            }
+        })
+    }
+}
